f1f128ee488a696685e3ca142058d255565436ac
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-02-14 17:04:27 +0500

    feat:  column

--- src/pages/Schedule/components/Column/index.tsx
+++ src/pages/Schedule/components/Column/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const Column = () => {
-  return (
-    <div>Column</div>
-  )
+import React from "react";
+interface ColumnProps {
+  id: number;
+  title: string;
+  data: any[];
 }
+const Column = ({ data, id, title }: ColumnProps) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="">
+        {data.map((e) => {
+          <p>test</p>;
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Column
+export default Column;
