dc9687d245b7b9f5a61b9215157655df45ce15a5
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-03-15 08:16:15 +0500

    feat: fonts and global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import { withProviders } from "./providers";
 import "./styles/index.scss";
-import "./styles/Vars.scss";
 
 const App = (children: React.ReactNode) => {
   return <main className="app">{children}</main>;
